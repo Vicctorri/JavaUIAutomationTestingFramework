c06c82807b78e0cf2a9bc3ca8216240f1c4276ac
Victoria <v.gorceac@developmentaid.org>
2024-01-05 10:13:16 +0200

    Implement the DriverManager

--- src/main/java/com/opencart/TestRunner.java
+++ src/main/java/com/opencart/TestRunner.java
@@ -1,17 +1,29 @@
 package com.opencart;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class TestRunner {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import com.opencart.managers.DriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class TestRunner {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = DriverManager.getInstance().getDriver();
+
+        String currentWindowName = driver.getWindowHandle();
+
+        //new window
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.get("https://www.google.com/");
+        Thread.sleep(2000);
+        System.out.println(driver.getTitle());
+        driver.close();
+
+        driver.switchTo().window(currentWindowName);
+        driver.get("https://tekwill.md/course/manual-quality-assurance/");
+        Thread.sleep(1000);
+        System.out.println(driver.getTitle());
+        driver.quit();
+
+        System.out.println("That's it!");
+
     }
 }
